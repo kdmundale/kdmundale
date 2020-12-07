369c82d61ca36709356ba6a470e58007b09c5d62
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-12-07 00:20:25 -0500

    total rework of page

--- script.js
+++ script.js
@@ -28,12 +28,6 @@ window.onclick = function(event) {
     slide = 0,
     moving = true;
 
-  const itemClassName2 = "carousel__photo2";
-    items2 = d.getElementsByClassName(itemClassName2),
-    totalItems2 = items2.length,
-    slide2 = 0;
-    moving = true;
-
     // Set classes
   function setInitialClasses() {
     // Targets the previous, current, and next items
@@ -41,26 +35,12 @@ window.onclick = function(event) {
     items[0].classList.add("active");
     items[1].classList.add("next");
   }
-
-  function setInitialClasses2() {
-    items2[totalItems2 - 1].classList.add("prev");
-    items2[0].classList.add("active");
-    items2[1].classList.add("next");
-  }
-
   // Set event listeners
   function setEventListeners() {
     let next = d.getElementsByClassName('carousel__button--next')[0],
         prev = d.getElementsByClassName('carousel__button--prev')[0];
     next.addEventListener('click', moveNext);
     prev.addEventListener('click', movePrev);
-  }
-
-  function setEventListeners2() {
-    let next2 = d.getElementsByClassName('carousel__button2--next')[0],
-        prev2 = d.getElementsByClassName('carousel__button2--prev')[0];
-    next2.addEventListener('click', moveNext2);
-    prev2.addEventListener('click', movePrev2);
   }
 
   // Next navigation handler
@@ -77,17 +57,6 @@ window.onclick = function(event) {
     }
   }
 
-  function moveNext2() {
-    if(!moving) {
-      if (slide2 === (totalItems2 - 1)) {
-        slide2=0;
-      } else {
-        slide2++;
-      }
-      // Move carousel to updated slide
-      moveCarousel2To(slide2);
-    }
-  }
   // Previous navigation handler
   function movePrev() {
     // Check if moving
@@ -99,19 +68,6 @@ window.onclick = function(event) {
         slide--;
       }
       moveCarouselTo(slide);
-    }
-  }
-
-  function movePrev2() {
-
-    if (!moving) {
-      if (slide2 === 0) {
-        slide2 = (totalItems2 - 1);
-      } else {
-        slide2--;
-      }
-      // Move carousel to updated slide
-      moveCarousel2To(slide2);
     }
   }
 
@@ -167,52 +123,9 @@ window.onclick = function(event) {
     }
   }
 
-  function moveCarousel2To(slide2) {
-    // Check if carousel is moving, if not, allow interaction
-    if(!moving) {
-      // temporarily disable interactivity
-      disableInteraction();
-      // Update the "old" adjacent slides with "new" ones
-      let newPrevious2 = slide2 - 1,
-          newNext2 = slide2 + 1,
-          oldPrevious2 = slide2 - 2,
-          oldNext2 = slide2 + 2;
-      // Test if carousel has more than three items
-      if ((totalItems2 - 1) > 3) {
-        // Checks and updates if the new slides are out of bounds
-        if (newPrevious2 <= 0) {
-          oldPrevious2 = (totalItems2 - 1);
-        } else if (newNext2 >= (totalItems2 - 1)){
-          oldNext2 = 0;
-        }
-        // Checks and updates if slide is at the beginning/end
-        if (slide2 === 0) {
-          newPrevious2 = (totalItems2 - 1);
-          oldPrevious2 = (totalItems2 - 2);
-          oldNext2 = (slide2 + 1);
-        } else if (slide2 === (totalItems2 -1)) {
-          newPrevious2 = (slide2 - 1);
-          newNext2 = 0;
-          oldNext2 = 1;
-        }
-        // Now we've worked out where we are and where we're going,
-        // by adding/removing classes we'll trigger the transitions.
-        // Reset old next/prev elements to default classes
-        items2[oldPrevious2].className = itemClassName2;
-        items2[oldNext2].className = itemClassName2;
-        // Add new classes
-        items2[newPrevious2].className = itemClassName2 + " prev";
-        items2[slide2].className = itemClassName2 + " active";
-        items2[newNext2].className = itemClassName2 + " next";
-      }
-    }
-  }
-
   function initCarousel() {
     setInitialClasses();
-    setInitialClasses2();
     setEventListeners();
-    setEventListeners2();
     // Set moving to false so that the carousel becomes interactive
     moving = false;
   }
